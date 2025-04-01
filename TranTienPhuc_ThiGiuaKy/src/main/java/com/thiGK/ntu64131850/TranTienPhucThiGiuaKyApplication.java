771351608e1dd5ed4b98c816c7ba8d7b7e9e91d1
Trần Tiến Phúc <112532197+tttpppppp@users.noreply.github.com>
2025-04-01 09:07:24 +0700

    first commit

---
+++ TranTienPhuc_ThiGiuaKy/src/main/java/com/thiGK/ntu64131850/TranTienPhucThiGiuaKyApplication.java
@@ -0,0 +1,13 @@
+package com.thiGK.ntu64131850;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranTienPhucThiGiuaKyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranTienPhucThiGiuaKyApplication.class, args);
+	}
+
+}
